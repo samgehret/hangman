c55f5f3fd86fa024751f8a77a682b8594f8a530c
Sam Gehret <sam.gehret@gmail.com>
2018-02-13 13:36:00 -0500

    moved logic into function, error handling, and form submission improvements

--- hangman.js
+++ hangman.js
@@ -1,14 +1,16 @@
-window.onbeforeunload = function () {
-  return 'you can not refresh the page'
-}
+// window.onbeforeunload = function () {
+//   return 'you can not refresh the page'
+// }
 
-var wordArray = prompt('please enter the secret word').split('')
+// capture the word to be guessed
+var wordArray = prompt('please enter the secret word').toLowerCase().split('')
 var submit = document.getElementById('submit')
 var form = document.getElementById('form')
 
 var guessedWrongArray = []
 var guessedRightArray = []
 
+// go through word to be guessed and create blank spots based on number of letters
 for (var i = 0; i < wordArray.length; i++) {
   var box = document.createElement('div')
   box.className = `box box${i}`
@@ -16,13 +18,14 @@ for (var i = 0; i < wordArray.length; i++) {
   letterBoard.appendChild(box)
 }
 
+// create an array with empty spots for the same length as the input word
 for (var j = 0; j < wordArray.length; j++) {
   guessedRightArray.push('')
 }
 
-// event listener on the submit button
-submit.addEventListener('click', (e) => {
-    var checker = []
+// define function to check the letter in the input box
+function checkLetter () {
+  var checker = []
 // this for loop will take the value input in the box and compare to every word
 // in the wordArray. If a match, it will store the letter in an array called checker
   for (var i = 0; i < wordArray.length; i++) {
@@ -30,14 +33,18 @@ submit.addEventListener('click', (e) => {
       var compareBox = document.querySelector('.box' + i)
       compareBox.innerHTML = form.value
       checker.push(form.value)
+      // when it finds a match, it will push the guessed into the
+      //below array. This is to keep track of which letter in the input word
+      // was guessed.
       guessedRightArray[i] = form.value
       if (guessedRightArray.toString() === wordArray.toString()) {
         alert('you win!!!!')
-      }
+      } 
     }
   }
   // if checker length = zero, it means no match was found.
   if (checker.length === 0) {
+    // add a box for a wrong guess and put the incorrectly guessed letter inside
     var box = document.createElement('div')
     box.className = 'box'
     var guessedLetters = document.querySelector('.guessedletters')
@@ -47,9 +54,23 @@ submit.addEventListener('click', (e) => {
     console.log(guessedWrongArray.length)
     var indexSelector = document.querySelector(`.index${guessedWrongArray.length}`)
     indexSelector.classList.remove('hidden')
+    form.value = ''
     // console.log(`.index${guessedWrongArray.length}`)
     if (guessedWrongArray.length === 6) {
       alert('you lose')
     }
+  }
+  form.value = ''
+}
+
+// event listener on the submit button
+submit.addEventListener('click', (e) => {
+  checkLetter()
+})
+
+// event listener for enter key
+form.addEventListener('keypress', (e) => {
+  if (e.keyCode === 13) {
+    checkLetter()
   }
 })
