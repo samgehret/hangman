821cf5680b76daab613a1b33c1c68cef2bf8c985
Sam Gehret <sam.gehret@gmail.com>
2018-02-13 20:48:17 -0500

    fixed edge case  where game can continue after win/loss

--- hangman.js
+++ hangman.js
@@ -89,39 +89,40 @@ function checkLoss () {
 
 // define function to check the letter in the input box
 function checkLetter () {
-  var checker = []
+// ADD SOMETHIING FOR IF ENDMESSAGE HAS CHILD
+  if (!endMessage.hasChildNodes()) {
+    var checker = []
 // this for loop will take the value input in the box and compare to every word
 // in the wordArray. If a match, it will store the letter in an array called checker
-  for (var i = 0; i < wordArray.length; i++) {
-    if (form.value === wordArray[i]) {
-      var compareBox = document.querySelector('.box' + i)
-      compareBox.innerHTML = form.value
-      checker.push(form.value)
+    for (var i = 0; i < wordArray.length; i++) {
+      if (form.value === wordArray[i]) {
+        var compareBox = document.querySelector('.box' + i)
+        compareBox.innerHTML = form.value
+        checker.push(form.value)
       // when it finds a match, it will push the guessed into the
       // below array. This is to keep track of which letter in the input word
       // was guessed.
-      guessedRightArray[i] = form.value
-      checkWin()
+        guessedRightArray[i] = form.value
+        checkWin()
+      }
     }
-  }
 
   // if checker length = zero, it means no match was found.
-  if (checker.length === 0) {
+    if (checker.length === 0 && wordArray.length > 0) {
     // add a box for a wrong guess and put the incorrectly guessed letter inside
-    var box = document.createElement('div')
-    box.className = 'box'
-    var guessedLetters = document.querySelector('.guessedletters')
-    box.innerHTML = form.value
-    guessedLetters.appendChild(box)
-    guessedWrongArray.push(form.value)
-    var indexSelector = document.querySelector(`.index${guessedWrongArray.length}`)
-    indexSelector.classList.remove('hidden')
-    checkLoss()
-    // console.log(`.index${guessedWrongArray.length}`)
+      var box = document.createElement('div')
+      box.className = 'box'
+      var guessedLetters = document.querySelector('.guessedletters')
+      box.innerHTML = form.value
+      guessedLetters.appendChild(box)
+      guessedWrongArray.push(form.value)
+      var indexSelector = document.querySelector(`.index${guessedWrongArray.length}`)
+      indexSelector.classList.remove('hidden')
+      checkLoss()
+    }
+    form.value = ''
   }
-  form.value = ''
 }
-  // clear input box after a guess is made
 
 // event listener on the submit button
 submit.addEventListener('click', (e) => {
